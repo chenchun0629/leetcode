c430e343dd16583d8381eca3729aa74684803330
chenchun <pangou@live.cn>
2021-12-20 14:19:17 +0800

    feat: 电话号码的字母组合

--- editor/cn/letter_combinations_of_a_phone_number.go
+++ editor/cn/letter_combinations_of_a_phone_number.go
@@ -58,6 +58,46 @@ func letterCombinations(digits string) []string {
 			'8': {"t", "u", "v"},
 			'9': {"w", "x", "y", "z"},
 		}
+		fn func(digits string, idx int, prefix []string) []string
+	)
+
+	if len(digits) == 0 {
+		return []string{}
+	}
+
+	fn = func(digits string, idx int, prefix []string) []string {
+		if idx == len(digits) {
+			return prefix
+		}
+
+		var ss []string
+
+		for _, pv := range prefix {
+			for _, sv := range m[digits[idx]] {
+				ss = append(ss, pv+sv)
+			}
+		}
+
+		return fn(digits, idx+1, ss)
+	}
+
+	return fn(digits, 1, m[digits[0]])
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+func letterCombinations_A(digits string) []string {
+	var (
+		m = map[byte][]string{
+			'2': {"a", "b", "c"},
+			'3': {"d", "e", "f"},
+			'4': {"g", "h", "i"},
+			'5': {"j", "k", "l"},
+			'6': {"m", "n", "o"},
+			'7': {"p", "q", "r", "s"},
+			'8': {"t", "u", "v"},
+			'9': {"w", "x", "y", "z"},
+		}
 		fn  func(digits string, idx int, prefix string)
 		ret []string
 	)
@@ -81,5 +121,3 @@ func letterCombinations(digits string) []string {
 
 	return ret
 }
-
-//leetcode submit region end(Prohibit modification and deletion)
