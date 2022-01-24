0036f683610b58684232dee91adeb2477bc30012
chenchun <pangou@live.cn>
2022-01-24 10:45:24 +0800

    feat: [700]二叉搜索树中的搜索

---
+++ editor/cn/search_in_a_binary_search_tree.go
@@ -0,0 +1,59 @@
+package main
+
+func main() {
+
+}
+
+//给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+//
+// 例如，
+//
+//
+//给定二叉搜索树:
+//
+//        4
+//       / \
+//      2   7
+//     / \
+//    1   3
+//
+//和值: 2
+//
+//
+// 你应该返回如下子树:
+//
+//
+//      2
+//     / \
+//    1   3
+//
+//
+// 在上述示例中，如果要找的值是 5，但因为没有节点值为 5，我们应该返回 NULL。
+// Related Topics 树 二叉搜索树 二叉树 👍 233 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil || root.Val == val {
+		return root
+	}
+
+	if t := searchBST(root.Left, val); t != nil {
+		return t
+	}
+
+	if t := searchBST(root.Right, val); t != nil {
+		return t
+	}
+
+	return nil
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
